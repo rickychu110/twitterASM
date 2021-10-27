9f09f9e27de3cc3e4a2af7149cdf1107563ac0d3
kcWu <79691025+kcwu229@users.noreply.github.com>
2021-10-28 02:06:13 +0800

    Delete Twitter_GUI directory

--- Twitter_GUI/per_confidential.py
+++
@@ -1,8 +0,0 @@
-consumer_key = "hqpqsemDOOpW1uLUatxhF6dbH"
-consumer_secret = "f6wbsfIsDGInCsBXDAFPjfMU2Zc5qT2nTZhHKmjUVMUOsm1RUB"
-
-access_token = "1449802836942807051-FIT24Nh8Rb6P9f2AtpZodHtB7nyY7N"
-access_token_secret = "oZOc0DfnDQHt7TJPfg7deNDa2ZsIuRlMXZTQ4YoFRiGju"
-
-bear_token = "AAAAAAAAAAAAAAAAAAAAAH4kUwEAAAAAqRPe3YhOvMi8deKPrxRPtue3dHI%3DL7y7gRmbBVxST5VN996jjFASHCQj2JaqvgDMXb0SXZP4obObrr"
-
